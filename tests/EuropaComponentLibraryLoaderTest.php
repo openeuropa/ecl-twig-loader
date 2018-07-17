9e2c291d224fdeb99a11d6bc205df618bc10dd73
Imanol Eguskiza <ier@opendeusto.es>
2018-07-17 16:05:10 +0200

    OPENEUROPA-710: Add code-review and fix coding standard errors.

--- tests/EuropaComponentLibraryLoaderTest.php
+++ tests/EuropaComponentLibraryLoaderTest.php
@@ -1,42 +1,49 @@
 <?php
 
+namespace OpenEuropa\Twig\Loader\Tests;
+
 use OpenEuropa\Twig\Loader\EuropaComponentLibraryLoader;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Class EuropaComponentLibraryLoaderTest
  */
-class EuropaComponentLibraryLoaderTest extends TestCase {
+class EuropaComponentLibraryLoaderTest extends TestCase
+{
 
-  /**
-   * Test loader.
-   *
-   * @dataProvider dataProvider
-   */
-  public function testLoader($name, $expected) {
-    $namespaces = [
-      'namespace1',
-      'namespace2',
-    ];
-    $loader = new EuropaComponentLibraryLoader($namespaces, 'tests/fixtures/components');
-    $twig = new Twig_Environment($loader);
-    $actual = $twig->loadTemplate($name)->render([]);
-    $actual = str_replace("\n", ' ', trim($actual));
-    $this->assertEquals($expected, $actual);
-  }
+    /**
+     * Test loader.
+     *
+     * @param string $name
+     * @param string $expected
+     *
+     * @dataProvider dataProvider
+     */
+    public function testLoader($name, $expected)
+    {
+        $namespaces = [
+            'namespace1',
+            'namespace2',
+        ];
+        $loader = new EuropaComponentLibraryLoader($namespaces, 'tests/fixtures/components');
+        $twig = new Twig_Environment($loader);
+        $actual = $twig->loadTemplate($name)->render([]);
+        $actual = str_replace("\n", ' ', trim($actual));
+        $this->assertEquals($expected, $actual);
+    }
 
-  /**
-   * Data provider.
-   *
-   * @return array
-   *    Test data.
-   */
-  public function dataProvider() {
-    return [
-      ['@namespace1/ecl-component1', 'ecl-component1.twig ecl-component2.twig ecl-component3.twig'],
-      ['@namespace2/ecl-component2', 'ecl-component2.twig ecl-component3.twig'],
-      ['@namespace2/ecl-component3', 'ecl-component3.twig'],
-    ];
-  }
-
+    /**
+     * Data provider.
+     *
+     * @return array
+     *    Test data.
+     */
+    public function dataProvider()
+    {
+        return [
+            ['@namespace1/ecl-component1', 'ecl-component1.twig ecl-component2.twig ecl-component3.twig'],
+            ['@namespace2/ecl-component2', 'ecl-component2.twig ecl-component3.twig'],
+            ['@namespace2/ecl-component3', 'ecl-component3.twig'],
+        ];
+    }
 }
